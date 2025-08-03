aa2c24451cdf7749ef7778f21853fdc18074976b
Kiboiko <kirboy2013@gmail.com>
2025-08-03 17:26:26 +0300

    update form

--- src/Components/Layout/AddAssetForm.jsx
+++ src/Components/Layout/AddAssetForm.jsx
@@ -1,6 +1,28 @@
 import { useState } from "react";
-import { Select, Space, Typography } from "antd";
+import {
+  Select,
+  Space,
+  Typography,
+  Flex,
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  Button,
+  DatePicker,
+} from "antd";
 import { useCrypto } from "../../Context/crypto-context";
+
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    number: "${label} is not valid number",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+
 export default function AddAssetForm() {
   const { crypto } = useCrypto();
   const [coin, setCoin] = useState(null);
@@ -25,11 +47,69 @@ export default function AddAssetForm() {
       />
     );
   }
+
+  function onFinish(values) {
+    console.log("finish", values);
+  }
+
   return (
-    <form>
-      <Typography.Title level={2} style={{ margin: 0 }}>
-        {coin.name}
-      </Typography.Title>
-    </form>
+    <Form
+      name="basic"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 10 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{
+        price: coin.price.toFixed(2),
+      }}
+      onFinish={onFinish}
+      validateMessages={validateMessages}
+    >
+      <Flex align="center">
+        <img
+          src={coin.icon}
+          alt={coin.name}
+          style={{ width: 40, marginRight: 10 }}
+        />
+        <Typography.Title level={2} style={{ margin: 0 }}>
+          {coin.name}
+        </Typography.Title>
+      </Flex>
+      <Divider />
+
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[
+          {
+            required: true,
+            type: "number",
+            min: 0,
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Enter coin amount"
+          style={{ width: "100%" }}
+        />
+      </Form.Item>
+
+      <Form.Item label="Price" name="price">
+        <InputNumber disabled style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item label="Date & Time" name="date">
+        <DatePicker showTime />
+      </Form.Item>
+
+      <Form.Item label="Total" name="total">
+        <InputNumber disabled style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Add Asset
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
