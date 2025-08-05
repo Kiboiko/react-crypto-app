4708b654319f8444d7b2cc0af9b0d5bb7371f228
Kiboiko <kirboy2013@gmail.com>
2025-08-06 01:10:31 +0300

    implementation of schedules

--- src/Components/Layout/AppContent.jsx
+++ src/Components/Layout/AppContent.jsx
@@ -1,5 +1,7 @@
-import { Layout } from "antd";
-
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../Context/crypto-context";
+import PortfolioChart from "./PortfolioChart";
+import AssetsTable from "./AssetsTable";
 const contentStyle = {
   textAlign: "center",
   minHeight: "calc(100vh - 60px)",
@@ -8,5 +10,26 @@ const contentStyle = {
 };
 
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price;
+    return acc;
+  }, {});
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: "left", color: "#fff" }}>
+        Portfolio:{" "}
+        {assets
+          .map((asset) => {
+            return asset.amount * cryptoPriceMap[asset.id];
+          })
+          .reduce((acc, val) => (acc += val), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+      <PortfolioChart />
+      <AssetsTable />
+    </Layout.Content>
+  );
 }
